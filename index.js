ec3ad227fd00df62240e3f3daf96022a80cf40ec
sakib5452 <najmus191@gmail.com>
2023-05-03 09:54:48 +0600

    data

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chef = require('./data/chef.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-}) 
+})  
