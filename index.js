abd2deb042de3356db048df00f69be55fa596b41
sakib5452 <najmus191@gmail.com>
2023-05-04 10:43:02 +0600

    res

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.get('/chef', (req, res) => {
 app.get('/chef/:id', (req, res) => {
     const id = req.params.id;
     const selectedChef = chef.find(n => n.id === id);
+    res.send(selectedChef)
 })
 
 app.listen(port, () => {
